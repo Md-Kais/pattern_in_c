97d0990c052a79258c6e8399f207a416e34fb833
Md-Kais <mdkais3@gmail.com>
2021-09-18 02:21:32 +0600

    if (i == 1 || j == 1 || i == n || j == n)

--- rhombus.c
+++ rhombus.c
@@ -1,1 +1,28 @@
-#include
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the n\n");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        //space in the front
+        for (int space = 0; space < n - i; space++)
+        {
+            printf(" ");
+        }
+        //* in the middle
+        for (int j = 1; j <= n; j++)
+        {
+            if (i == 1 || j == 1 || i == n || j == n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
